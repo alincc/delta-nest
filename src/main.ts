cb741033e2ad9c9ffd4da52d67ca083e29cb55c9
Miguel Romero <miguelromereado@outlook.com>
2020-02-09 15:41:19 -0600

    cors ionic

--- src/main.ts
+++ src/main.ts
@@ -6,31 +6,10 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: false,
-    cors: {
-      origin: "*",
-      preflightContinue: true,
-      allowedHeaders: [
-        "Content-Type",
-        "Accept",
-        "Authorization",
-        "Access-Control-Allow-Origin",
-        "origin",
-        "Access-Control-Allow-Methods"
-      ],
-      optionsSuccessStatus: 200
-    }
+    logger: false
   });
 
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Accept, Authorization, Access-Control-Allow-Origin, origin, Access-Control-Allow-Methods"
-    );
-    next();
-  });
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
 
